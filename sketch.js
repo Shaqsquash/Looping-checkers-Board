932123bb4a07587e949a23dc8260f3f3faab4911
Eric <ericv53128@students.laalliance.org>
2017-11-17 17:04:10 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,124 @@
+function setup() {
+  createCanvas(500,600)
+}
+
+function draw() {
+  background('gray')
+
+  var x = 5
+
+
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===0) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 100, 40, 40)
+   x += 40
+   
+ }
+ 
+ x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===1) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 140, 40, 40)
+   x += 40
+   
+ } 
+
+ 
+ x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===0) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 180, 40, 40)
+   x += 40
+   
+ } 
+ 
+  
+ x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===1) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 220, 40, 40)
+   x += 40
+   
+ } 
+ 
+  x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===0) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 260, 40, 40)
+   x += 40
+   
+ } 
+ 
+  x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===1) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 300, 40, 40)
+   x += 40
+   
+ } 
+ 
+  x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===0) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 340, 40, 40)
+   x += 40
+   
+ } 
+ 
+  x = 5
+ for (var count = 0; count < 8; count+= 1) {
+ 
+  if (count%2===1) {
+   fill ("blue")
+  }
+  else  {
+   fill("red")
+  }
+   rect(x, 380, 40, 40)
+   x += 40
+   
+ } 
+ 
+}
